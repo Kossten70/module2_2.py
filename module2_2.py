8a843771c394de4900e6d3cd801fdb9ab0bd783a
Konstantin <ref6512722@gmail.com>
2024-09-24 21:15:00 +0300

    01

--- module2_2.py
+++ module2_2.py
@@ -3,7 +3,7 @@ second = int(input('Введите число 2:'))
 third = int(input('Введите число 3:'))
 if first==second==third:
     print(3)
-elif first!=second!=third:
+elif not first==second==third:
     print(0)
 elif first!=second or second!=third or first!=third:
     print(2)
